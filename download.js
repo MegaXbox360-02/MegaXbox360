67dc3fc1e33d473cd58e3e53d0738fb641fd3ff7
MegaXbox360-02 <megaxbox360isos2@gmail.com>
2025-04-06 19:57:38 -0300

    Update download.js

--- download.js
+++ download.js
@@ -1,9 +1,10 @@
-// Obtém o parâmetro "publicacao" da URL
+// Obtém os parâmetros "categoria" e "publicacao" da URL
 const params = new URLSearchParams(window.location.search);
+const categoria = params.get("categoria");
 const publicacao = params.get("publicacao");
 
-// Carrega o arquivo JSON correspondente à publicação
-fetch(`dados/publicacao-${publicacao}.json`)
+// Carrega o arquivo JSON correspondente à categoria e publicação
+fetch(`dados/${categoria}/${categoria}-${publicacao}.json`)
   .then((response) => response.json())
   .then((dados) => {
     // Substitui os placeholders pelos dados da publicação
